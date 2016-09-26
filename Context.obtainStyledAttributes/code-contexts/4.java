26f055501528292fd99ebde159fcdf32b25fe287
eduardocunha11 <eduardocunha11@gmail.com>
2016-09-26 17:11:45 -0300

    Create 4.java

---
+++ Context.obtainStyledAttributes/code-contexts/4.java
@@ -0,0 +1,21 @@
+/**
+ * Source: https://github.com/zjuhjz/YACleaner/blob/master/src/com/nineoldandroids/animation/AnimatorInflater.java
+ */ 
+private static ObjectAnimator loadObjectAnimator(Context context, AttributeSet attrs)
+            throws NotFoundException {
+
+        ObjectAnimator anim = new ObjectAnimator();
+
+        loadAnimator(context, attrs, anim);
+
+        TypedArray a =
+                context.obtainStyledAttributes(attrs, /*com.android.internal.R.styleable.*/PropertyAnimator);
+
+        String propertyName = a.getString(/*com.android.internal.R.styleable.*/PropertyAnimator_propertyName);
+
+        anim.setPropertyName(propertyName);
+
+        a.recycle();
+
+        return anim;
+}
