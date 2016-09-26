e0ecb58371894f6cb6b78617561911278679305a
eduardocunha11 <eduardocunha11@gmail.com>
2016-09-26 16:06:22 -0300

    Create 1.java

---
+++ Environment.getExternalStorageState/code-contexts/1.java
@@ -0,0 +1,20 @@
+/**
+ * Source: https://github.com/17up/17Card/blob/master/plugins/org.apache.cordova.camera/src/android/CameraLauncher.java
+ */
+private String getTempDirectoryPath() {
+        File cache = null;
+
+        // SD Card Mounted
+        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            cache = new File(Environment.getExternalStorageDirectory().getAbsolutePath() +
+                    "/Android/data/" + cordova.getActivity().getPackageName() + "/cache/");
+        }
+        // Use internal storage
+        else {
+            cache = cordova.getActivity().getCacheDir();
+        }
+
+        // Create the cache directory if it doesn't exist
+        cache.mkdirs();
+        return cache.getAbsolutePath();
+}
