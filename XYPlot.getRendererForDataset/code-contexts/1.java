c6fd5c8977cfca983d32ee4899b354d78e418e68
eduardocunha11 <eduardocunha11@gmail.com>
2016-09-29 16:56:59 -0300

    Create 1.java

---
+++ XYPlot.getRendererForDataset/code-contexts/1.java
@@ -0,0 +1,23 @@
+/**
+ * Source: https://github.com/DanielDong/I2mine_Project/blob/master/jfreechart-1.0.14/tests/org/jfree/chart/plot/junit/XYPlotTests.java
+ */ 
+public void testGetRendererForDataset() {
+        XYDataset d0 = new XYSeriesCollection();
+        XYDataset d1 = new XYSeriesCollection();
+        XYDataset d2 = new XYSeriesCollection();
+        XYDataset d3 = new XYSeriesCollection();  // not used by plot
+        XYItemRenderer r0 = new XYLineAndShapeRenderer();
+        XYItemRenderer r2 = new XYLineAndShapeRenderer();
+        XYPlot plot = new XYPlot();
+        plot.setDataset(0, d0);
+        plot.setDataset(1, d1);
+        plot.setDataset(2, d2);
+        plot.setRenderer(0, r0);
+        // no renderer 1
+        plot.setRenderer(2, r2);
+        assertEquals(r0, plot.getRendererForDataset(d0));
+        assertEquals(r0, plot.getRendererForDataset(d1));
+        assertEquals(r2, plot.getRendererForDataset(d2));
+        assertEquals(null, plot.getRendererForDataset(d3));
+        assertEquals(null, plot.getRendererForDataset(null));
+}
