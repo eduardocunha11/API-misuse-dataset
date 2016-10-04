cffe7a5b4e1a6bd169668a3f12b46a5a343cb5cd
eduardocunha11 <eduardocunha11@gmail.com>
2016-10-04 10:14:17 -0300

    Update 1.java

--- File.createNewFile/code-contexts/1.java
+++ File.createNewFile/code-contexts/1.java
@@ -5,19 +5,18 @@ public void makeSelector(String resPath,boolean reWrite) throws IOException {
     
     ...
 
-    for(String validDrawableName:validSelectAbleDrawables)
-		{
-			  File selectorFile=new File(drawableDir,validDrawableName+".xml");
-			  if (selectorFile.exists()) {
-				   System.err.println(validDrawableName+" has already exists in drawable directory");
-				   if(!reWrite) continue; 
-			  }
-			  selectorFile.createNewFile();
-			  Properties attribute=new Properties();
-			  attribute.put(KEY_FOCUS_DRAWABLE, validDrawableName+split+focusSuffix);
-			  attribute.put(KEY_NORMAL_DRAWABLE, validDrawableName+split+normalSuffix);
-			  String makedSelector=TemplateParser.parseTemplate(TEMPLATE_SELECTOR, attribute);
-			  Files.write(selectorFile, makedSelector);
-			  System.out.println("make selector:"+validDrawableName+" complete!");
+    for(String validDrawableName:validSelectAbleDrawables) {
+	File selectorFile=new File(drawableDir,validDrawableName+".xml");
+	if (selectorFile.exists()) {
+	   System.err.println(validDrawableName+" has already exists in drawable directory");
+	   if(!reWrite) continue; 
+	}
+	selectorFile.createNewFile();
+	Properties attribute=new Properties();
+	attribute.put(KEY_FOCUS_DRAWABLE, validDrawableName+split+focusSuffix);
+	attribute.put(KEY_NORMAL_DRAWABLE, validDrawableName+split+normalSuffix);
+	String makedSelector=TemplateParser.parseTemplate(TEMPLATE_SELECTOR, attribute);
+	Files.write(selectorFile, makedSelector);
+	System.out.println("make selector:"+validDrawableName+" complete!");
     }
 }
