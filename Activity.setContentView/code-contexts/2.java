8f1ed4686ac07cac46c371aac00a035da0b3792a
eduardocunha11 <eduardocunha11@gmail.com>
2016-10-06 10:16:09 -0300

    Create 2.java

---
+++ Activity.setContentView/code-contexts/2.java
@@ -0,0 +1,9 @@
+/**
+ * Source: https://github.com/HasegawaTadamitsu/ExampleAndroid/blob/master/location-app/src/main/java/jp/ddo/haselab/location/example1/main/GPSActivity.java
+ */
+protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        MyLog.getInstance().verbose("start");
+        setContentView(R.layout.gps);
+}
